a2ecc0ef198b2d5d50cbe3cf5cb9e717214bd3aa
Loic699 <loicpimet@icloud.com>
2021-10-22 16:42:07 +0200

    first commit

---
+++ src/AudioPlayer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './components/graphics/Header'
+
+import Playlist from './components/playlist/Playlist'
+import Actions from './components/playlist/Actions'
+import Controls from './components/Controls'
+
+import PlayerState from './context/PlayerState'
+
+
+import './main.css'
+import './input.css'
+
+function AudioPlayer() {
+
+  return (
+    <PlayerState>
+      <div className="main">
+        <div className="top">
+          <div className="left">
+            <Header />
+          </div>
+          <div className="right">
+            {/* <Actions /> */}
+            {/* <Playlist /> */}
+          </div>
+        </div>
+        <Controls />
+      </div>
+    </PlayerState>
+  );
+}
+
+export default AudioPlayer;
